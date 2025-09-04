7cf4047bd5fde6cb4a5fc2c95de62bbac62057c0
daiki-ny <yakitori283@outlook.jp>
2025-09-04 15:25:30 +0900

    Initial commit

---
+++ src/main/java/com/example/my_portfolio_blog/MyPortfolioBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_portfolio_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortfolioBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPortfolioBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/my_portfolio_blog/MyPortfolioBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_portfolio_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortfolioBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
